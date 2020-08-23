a11cd0752d7341002c479be75f9e07569d218911
rturnbull <robert.turnbull@unimelb.edu.au>
2020-08-23 17:37:19 +1000

    adding iterations

--- models.py
+++ models.py
@@ -5,32 +5,43 @@ import gotoh_msa
 import numpy as np
 from jsonfield import JSONField
 from ndarray import NDArrayField
-
+from scipy.sparse import lil_matrix
+import matplotlib.pyplot as plt
 from dcodex.models import *
+from dcodex.strings import *
+
+def tokenize_string( string ):
+    string = string.replace("."," .")
+    string = re.sub("\s+"," ", string)
+    string = remove_markup(string)
+    return string.split()
 
 def tokenize_strings( transcriptions ):
-    return [transcription.transcription.split() for transcription in transcriptions]
+    return [tokenize_string(transcription.transcription) for transcription in transcriptions]
 
-def align_family_at_verse(family, verse, gotoh_param):
+def align_family_at_verse(family, verse, gotoh_param, iterations_count = 1, gap_open=-5, gap_extend=-2):
     transcriptions = list(family.transcriptions_at(verse))
+
+    #transcriptions = [t for t in transcriptions if t.manuscript.siglum in ["J67_esk", "S128_esk", "CSA"]]
 
     # Distance matrix
     distance_matrix_as_vector = []
     for x_index, x in enumerate(transcriptions):
+        x_string = normalize_transcription(x.transcription)
         for y_index, y in enumerate(transcriptions):
             if y_index >= x_index:
                 break
-
-            distance = gotoh_counts.weighted_nonmatches( x.transcription, y.transcription, *gotoh_param )
+            y_string = normalize_transcription(y.transcription)
+            distance = gotoh_counts.nonmatches( x_string, y_string, *gotoh_param )
             distance_matrix_as_vector.append( distance )
 
     # Guide tree
     method="average"
     linkage = hierarchy.linkage(distance_matrix_as_vector, method)
 
-    # Tokenise
+    # Tokenize
     token_strings = tokenize_strings(transcriptions)
-    print(token_strings)
+    #print(token_strings)
     vocab = {}
     vocab_index = 0
     alignments = []
@@ -46,8 +57,23 @@ def align_family_at_verse(family, verse, gotoh_param):
         #print(alignment)
         alignments.append( alignment )
 
+    id_to_word = np.asarray(list(vocab.keys()) )
 
-    # Alignment
+    # Scoring Matrix
+    n = len(id_to_word)
+    scoring_matrix = np.zeros( (n,n), dtype=np.float32 )
+    for index_i in range(n):
+        token_i = normalize_transcription(str(id_to_word[index_i]))
+        for index_j in range(index_i+1):
+            token_j = normalize_transcription(str(id_to_word[index_j]))
+            #print(index_i,index_j)
+            scoring_matrix[index_i,index_j] = gotoh_counts.score( token_i, token_j, *gotoh_param )
+    #print(scoring_matrix)
+    #print([scoring_matrix[1][2]])
+    #print([scoring_matrix[2][1]])
+    #return
+
+    # Initial Progressive Alignment
     alignment_transcriptions = [[transcription] for transcription in transcriptions]
     for link_index, link_row in enumerate(linkage):
         node_id = len(transcriptions) + link_index
@@ -56,22 +82,48 @@ def align_family_at_verse(family, verse, gotoh_param):
 
         left_alignment = alignments[left_node_id]
         right_alignment = alignments[right_node_id]
+        #print("--")
+        #print('left sigla', [t.manuscript.siglum for t in alignment_transcriptions[left_node_id]])
+        #print('right sigla', [t.manuscript.siglum for t in alignment_transcriptions[right_node_id]])
+#        return 
 
-        new_alignment = gotoh_msa.align( left_alignment, right_alignment, gap_open=-10, gap_extend=-1 )
+        new_alignment = gotoh_msa.align( left_alignment, right_alignment, matrix=scoring_matrix, gap_open=gap_open, gap_extend=gap_extend )
         alignments.append( new_alignment )
         alignment_transcriptions.append(  alignment_transcriptions[left_node_id] + alignment_transcriptions[right_node_id] )
+        #
+        # if link_index == 6:
+        #    break
 
     alignment_array = alignments[-1]
-    print("alignment", alignment.shape)
-    print("len(alignment_transcriptions[-1])", len(alignment_transcriptions[-1]))
+    alignment_transcriptions = alignment_transcriptions[-1]
 
-    print(alignment_transcriptions[-1])
+    for iteration in range(iterations_count):
+        for transcription in alignment_transcriptions:
+            #print(transcription.manuscript.siglum)
+            #print(alignment_array)
+            # Pop row
+            row = alignment_array[:,0]
+            row = np.delete( row, row < 0 ) # Remove all the gaps
+            #print(row)
+            row = np.expand_dims( row, axis=1)
+            alignment_array = alignment_array[:,1:]
+            # Realign row
+
+            alignment_array = gotoh_msa.align( alignment_array, row, matrix=scoring_matrix, gap_open=gap_open, gap_extend=gap_extend, visualize=False )
+            #print(alignment_array)
+            #if transcription.manuscript.siglum == "J67_esk":
+            #    return
+
+    #print("alignment", alignment.shape)
+    #print("len(alignment_transcriptions)", len(alignment_transcriptions))
+
+    #print(alignment_transcriptions)
+    
 
 
     alignment, _ = Alignment.objects.update_or_create( family=family, verse=verse, defaults={
         "word_to_id": vocab,
-        "id_to_word": np.asarray( list(vocab.keys()) ),
-        "matrix_data": np.asarray( list(vocab.keys()) ),
+        "id_to_word": id_to_word,
     } )
     #alignment = Alignment( family=family, verse=verse )
     #alignment.save()
@@ -80,26 +132,22 @@ def align_family_at_verse(family, verse, gotoh_param):
     #alignment.id_to_word = np.asarray( list(vocab.keys()) )
 
     id_to_word = np.asarray( list(vocab.keys()) )
-    print("id_to_word", id_to_word)
+    #print("id_to_word", id_to_word)
     #return
 
+    
     for order in range( alignment_array.shape[0] ):
         column, _ = Column.objects.update_or_create( order=order, alignment=alignment, defaults={} )
+    Column.objects.filter(alignment=alignment, order__gte=alignment_array.shape[0] ).delete()
 
-    for transcription, tokens in zip( alignment_transcriptions[-1], np.rollaxis(alignment_array, 1) ):
+    Row.objects.filter(alignment=alignment).delete()
+    for transcription, tokens in zip( alignment_transcriptions, np.rollaxis(alignment_array, 1) ):
         aligned_transcription, _ = Row.objects.update_or_create( transcription=transcription, alignment=alignment, defaults={
             "tokens": tokens,
         } )
 
-        # TODO Save Columns
-        
-        print("transcription", transcription)
-        print("tokens", tokens)
-        for token in tokens:
-            if token == -1:
-                print("GAP")
-            else:
-                print(token, id_to_word[token])
+    #dn = hierarchy.dendrogram(linkage, orientation='right',labels=[transcription.manuscript.siglum for transcription in transcriptions])
+    #plt.show()
 
 
 class Alignment(models.Model):
@@ -107,7 +155,6 @@ class Alignment(models.Model):
     verse = models.ForeignKey( Verse, on_delete=models.CASCADE )
     word_to_id = JSONField(help_text="Vocab dictionary")
     id_to_word = NDArrayField(help_text="Index of vocab dictionary")
-    matrix_data = NDArrayField(help_text="Numpy array for the similarity matrix")
 
 
 class Row(models.Model):
--- templatetags/dcodex_collation_tags.py
+++ templatetags/dcodex_collation_tags.py
@@ -6,4 +6,6 @@ register = template.Library()
 @register.filter
 def token(row, column):
     token_id = row.tokens[ column.order ]
+    if token_id < 0:
+        return ""
     return row.alignment.id_to_word[ token_id ]
