56ae34fef80340836123bef204b295204b041f63
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2021-06-02 09:03:17 +1000

    getting the unique readings command to ignore transitions if asked

--- dcodex_collation/management/commands/uniquereadings.py
+++ dcodex_collation/management/commands/uniquereadings.py
@@ -8,6 +8,21 @@ from dcodex_collation.models import *
 
 from django.core.management.base import BaseCommand, CommandError
 
+def get_equivalent_state_ids( states, transitions_to_process ):
+    """ Returns the set of state ids with the states with the lowest ids if transitions between states can be ignored. """
+    equivalent_state_ids = set()
+    for state in states:
+        while True:
+            if transition := transitions_to_process.filter(start_state=state, start_state__id__gt=F("end_state__id")).first():
+                state = transition.end_state
+            elif transition := transitions_to_process.filter(end_state=state, end_state__id__gt=F("start_state__id")).first():
+                state = transition.start_state
+            else:
+                break
+        equivalent_state_ids.update([state.id])
+    return equivalent_state_ids
+
+
 class Command(BaseCommand):
     help = "Finds unique readings for a group."
 
@@ -17,12 +32,13 @@ class Command(BaseCommand):
         parser.add_argument('--start', type=str, help="The starting verse of the passage selection.")
         parser.add_argument('--end', type=str, help="The ending verse of the passage selection. If this is not given, then it only aligns the start verse.")
         parser.add_argument('--file', type=str, help="An output file.")
+        parser.add_argument('-i', '--ignore', action='store_true', default=False, help="Whether it should ignore transisions in the TransitionsToIgnore group.")
 
     def handle(self, *args, **options):
         family = Family.objects.get(name=options['family'])
         witnesses_in_family = family.manuscripts()
 
-        allow_ignore = True
+        allow_ignore = options['ignore']
 
         sigla = options['sigla']
         mss_in_group = [Manuscript.find(siglum) for siglum in sigla]
@@ -53,6 +69,9 @@ class Command(BaseCommand):
 
         site = Site.objects.get_current()
         domain_name = site.domain
+        
+        if allow_ignore:
+            transition_type_ids_to_ignore = TransitionTypeToIgnore.objects.all().values_list("transition_type__id", flat=True)
 
         alignments = Alignment.objects.filter(family=family, verse__in=verses)
         for alignment in alignments:
@@ -69,7 +88,18 @@ class Command(BaseCommand):
 
                 intersection = states_ingroup & states_outgroup
                 if intersection.count() > 0:
+                    # if there is already an intersection between the ingroup and the outgroup then we don't have a unique reading 
+                    # Continue to the next column
                     continue
+
+                if allow_ignore:
+                    transitions_to_process = Transition.objects.filter(transition_type__id__in=transition_type_ids_to_ignore, column=column)
+                    
+                    equivalent_state_ids_ingroup = get_equivalent_state_ids( states_ingroup, transitions_to_process)
+                    equivalent_state_ids_outgroup = get_equivalent_state_ids( states_outgroup, transitions_to_process)
+
+                    if equivalent_state_ids_ingroup & equivalent_state_ids_outgroup:
+                        continue
 
                 for csv_writer in csv_writers:
                     csv_writer.writerow([
